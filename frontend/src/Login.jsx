8f4078ea383965794e1135a426340e8b40b2054c
Sanjeev Bhusal <bhusalsanjeev23@gmail.com>
2025-11-08 01:28:16 +0545

    make page responsive

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { Menu } from "lucide-react";
 import { Navigate, useNavigate } from "react-router";
 import { useAuth } from "./AuthContext";
 
 export default function Login() {
 	const { user } = useAuth();
-	const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+	const [isLanguageSwitcherDropdownOpen, setIsLanguageSwitcherDropdownOpen] =
+		useState(false);
+	const [isHamburgerMenuOpen, setIsHamburgerMenuOpen] = useState(false);
 	const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 	const [translations, setTranslations] = useState(null);
 	const [selectedLanguage, setSelectedLanguage] = useState({
@@ -35,14 +38,28 @@ export default function Login() {
 
 	useEffect(() => {
 		const handleClickOutside = (event) => {
-			if (isDropdownOpen && !event.target.closest("#language-dropdown")) {
-				setIsDropdownOpen(false);
+			if (
+				isLanguageSwitcherDropdownOpen &&
+				!event.target.closest("#language-dropdown")
+			) {
+				setIsLanguageSwitcherDropdownOpen(false);
 			}
 		};
 
 		document.addEventListener("click", handleClickOutside);
 		return () => document.removeEventListener("click", handleClickOutside);
-	}, [isDropdownOpen]);
+	}, [isLanguageSwitcherDropdownOpen]);
+
+	useEffect(() => {
+		const handleClickOutside = (event) => {
+			if (isHamburgerMenuOpen && !event.target.closest("#hamburger-menu")) {
+				setIsHamburgerMenuOpen(false);
+			}
+		};
+
+		document.addEventListener("click", handleClickOutside);
+		return () => document.removeEventListener("click", handleClickOutside);
+	}, [isHamburgerMenuOpen]);
 
 	if (user) {
 		return <Navigate to="/pricelist" replace />;
@@ -54,7 +71,7 @@ export default function Login() {
 
 	const handleLanguageSelect = (language) => {
 		setSelectedLanguage(language);
-		setIsDropdownOpen(false);
+		setIsLanguageSwitcherDropdownOpen(false);
 	};
 
 	const handleSubmit = (e) => {
@@ -127,74 +144,75 @@ export default function Login() {
 					<img
 						src="https://storage.123fakturera.se/public/icons/diamond.png"
 						alt="logo"
-						style={{ height: "2rem" }}
+						className="logo"
 					/>
-					<ul
-						style={{
-							display: "flex",
-							alignItems: "center",
-							gap: "60px",
-							margin: "0",
-						}}
-					>
-						<li
-							style={{
-								fontWeight: 500,
-								cursor: "pointer",
-								listStyle: "none",
-							}}
-						>
-							{translations.navBar.home}
+
+					<div className="hamburger-icon-container" id="hamburger-menu">
+						<Menu
+							className="hamburger-icon"
+							onClick={() => setIsHamburgerMenuOpen(!isHamburgerMenuOpen)}
+						/>
+						{isHamburgerMenuOpen && (
+							<div className="hamburger-menu-content">
+								<ul className="nav-list">
+									<li className="nav-list-item">
+										<a href="https://www.123fakturera.se/index.html">
+											{translations.navBar.home}
+										</a>
+									</li>
+									<li className="nav-list-item">
+										<a href="https://www.123fakturera.se/bestall.html">
+											{translations.navBar.order}
+										</a>
+									</li>
+									<li className="nav-list-item">
+										<a href="https://www.123fakturera.se/kunder.html">
+											{translations.navBar.ourCustomers}
+										</a>
+									</li>
+									<li className="nav-list-item">
+										<a href="https://www.123fakturera.se/omoss.html">
+											{translations.navBar.aboutUs}
+										</a>
+									</li>
+									<li className="nav-list-item">
+										<a href="https://www.123fakturera.se/kontaktaoss.html">
+											{translations.navBar.contactUs}
+										</a>
+									</li>
+								</ul>
+							</div>
+						)}
+					</div>
+
+					<ul className="nav-list-big-screen">
+						<li className="nav-list-item-big-screen">
+							<a href="https://www.123fakturera.se/index.html">
+								{translations.navBar.home}
+							</a>
 						</li>
-						<li
-							style={{
-								cursor: "pointer",
-								fontWeight: 500,
-								listStyle: "none",
-							}}
-						>
+						<li className="nav-list-item-big-screen">
 							{translations.navBar.order}
 						</li>
-						<li
-							style={{
-								cursor: "pointer",
-								fontWeight: 500,
-								listStyle: "none",
-							}}
-						>
+						<li className="nav-list-item-big-screen">
 							{translations.navBar.ourCustomers}
 						</li>
-						<li
-							style={{
-								cursor: "pointer",
-								fontWeight: 500,
-								listStyle: "none",
-							}}
-						>
+						<li className="nav-list-item-big-screen">
 							{translations.navBar.aboutUs}
 						</li>
-						<li
-							style={{
-								cursor: "pointer",
-								fontWeight: 500,
-								listStyle: "none",
-							}}
-						>
+						<li className="nav-list-item-big-screen">
 							{translations.navBar.contactUs}
 						</li>
 
 						{/* Language Dropdown */}
 						{/* biome-ignore lint: ignore all rules for this li */}
 						<li
-							style={{
-								cursor: "pointer",
-								fontWeight: 500,
-								listStyle: "none",
-								display: "flex",
-								alignItems: "center",
-								gap: "16px",
-							}}
-							onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+							className="nav-list-item-big-screen language-dropdown-big-screen"
+							onClick={() =>
+								setIsLanguageSwitcherDropdownOpen(
+									!isLanguageSwitcherDropdownOpen,
+								)
+							}
 							id="language-dropdown"
 						>
 							{selectedLanguage.name}
@@ -216,7 +234,7 @@ export default function Login() {
 										borderRadius: "20%",
 									}}
 								/>
-								{isDropdownOpen && (
+								{isLanguageSwitcherDropdownOpen && (
 									<div
 										style={{
 											position: "absolute",
@@ -294,116 +312,151 @@ export default function Login() {
 							</div>
 						</li>
 					</ul>
+
+					{/* Language Dropdown */}
+					{/* biome-ignore lint: ignore all rules for this li */}
+					<div
+						className="language-dropdown"
+						onClick={() =>
+							setIsLanguageSwitcherDropdownOpen(!isLanguageSwitcherDropdownOpen)
+						}
+						id="language-dropdown"
+					>
+						{selectedLanguage.name}
+
+						<div
+							style={{
+								cursor: "pointer",
+								width: "26px",
+								height: "18px",
+								position: "relative",
+							}}
+						>
+							<img
+								src={selectedLanguage.flag}
+								alt={selectedLanguage.alt}
+								style={{
+									width: "100%",
+									height: "100%",
+									borderRadius: "20%",
+								}}
+							/>
+							{isLanguageSwitcherDropdownOpen && (
+								<div
+									style={{
+										position: "absolute",
+										top: "28px",
+										right: "-6px",
+										borderRadius: "5px",
+										padding: "8px 0px",
+										backgroundColor: "white",
+										boxShadow: "0 2px 8px rgba(0,0,0,0.1)",
+										zIndex: 1000,
+									}}
+								>
+									{/* biome-ignore lint: ignore all rules for this div */}
+									<div
+										style={{
+											display: "flex",
+											alignItems: "center",
+											justifyContent: "space-between",
+											gap: "16px",
+											padding: "8px 16px",
+											cursor: "pointer",
+										}}
+										onClick={() =>
+											handleLanguageSelect({
+												name: "Svenska",
+												flag: "https://storage.123fakturere.no/public/flags/SE.png",
+												alt: "Swedish flag",
+											})
+										}
+									>
+										<span style={{ color: "black" }}>Svenska</span>
+										<img
+											src="https://storage.123fakturere.no/public/flags/SE.png"
+											alt="Swedish flag"
+											style={{
+												width: "26px",
+												height: "18px",
+												cursor: "pointer",
+												borderRadius: "20%",
+											}}
+										/>
+									</div>
+									{/* biome-ignore lint: ignore all rules for this div */}
+									<div
+										style={{
+											display: "flex",
+											alignItems: "center",
+											justifyContent: "space-between",
+											gap: "16px",
+											padding: "8px 16px",
+											cursor: "pointer",
+										}}
+										onClick={() =>
+											handleLanguageSelect({
+												name: "English",
+												flag: "https://storage.123fakturere.no/public/flags/GB.png",
+												alt: "English flag",
+											})
+										}
+									>
+										<span style={{ color: "black" }}>English</span>
+										<img
+											src="https://storage.123fakturere.no/public/flags/GB.png"
+											alt="English flag"
+											style={{
+												width: "26px",
+												height: "18px",
+												cursor: "pointer",
+												borderRadius: "20%",
+											}}
+										/>
+									</div>
+								</div>
+							)}
+						</div>
+					</div>
 				</header>
 			</nav>
 
 			{/* body */}
 
-			<div
-				style={{
-					display: "flex",
-					justifyContent: "center",
-					alignItems: "center",
-					height: "fit-content",
-					marginTop: "100px",
-				}}
-			>
-				<div
-					style={{
-						backgroundColor: "white",
-						color: "black",
-						padding: "24px 12px 40px 12px",
-						borderRadius: "40px",
-						width: "540px",
-						display: "flex",
-						flexDirection: "column",
-						alignItems: "center",
-					}}
-				>
-					<h1
-						style={{
-							fontWeight: "bold",
-							color: "red",
-							margin: 0,
-							fontSize: "2.5rem",
-						}}
-					>
-						{translations.login.loginText}
-					</h1>
+			<div className="login-card-container">
+				<div className="login-card">
+					<h1 className="login-text">{translations.login.loginText}</h1>
 
-					<form
-						onSubmit={handleSubmit}
-						style={{
-							width: "100%",
-							display: "flex",
-							flexDirection: "column",
-							alignItems: "center",
-						}}
-					>
-						<div
-							style={{
-								width: "80%",
-								display: "flex",
-								flexDirection: "column",
-								gap: "0.2rem",
-								marginTop: "1rem",
-							}}
-						>
-							<p style={{ margin: "0" }}>{translations.login.emailLabel}</p>
+					<form onSubmit={handleSubmit} className="login-form">
+						<div className="login-item">
+							<p className="login-item-label">
+								{translations.login.emailLabel}
+							</p>
 							<input
 								name="email"
 								type="email"
 								required
 								placeholder={translations.login.emailPlaceholder}
-								style={{
-									background: "white",
-									color: "black",
-									borderRadius: "1.5rem",
-									padding: "0 1rem",
-									height: "42px",
-									fontSize: "16px",
-								}}
+								className="login-item-input"
 							/>
 						</div>
 
-						<div
-							style={{
-								width: "80%",
-								display: "flex",
-								flexDirection: "column",
-								gap: "0.2rem",
-								marginTop: "1rem",
-							}}
-						>
-							<p style={{ margin: "0" }}>{translations.login.passwordLabel}</p>
-							<div style={{ position: "relative", width: "100%" }}>
+						<div className="login-item">
+							<p className="login-item-label">
+								{translations.login.passwordLabel}
+							</p>
+							<div className="password-input">
 								<input
 									name="password"
 									type={isPasswordVisible ? "text" : "password"}
 									required
 									placeholder={translations.login.passwordPlaceholder}
-									style={{
-										background: "white",
-										color: "black",
-										borderRadius: "1.5rem",
-										padding: "0 1rem",
-										height: "42px",
-										width: "100%",
-										fontSize: "16px",
-									}}
+									className="login-item-input"
 								/>
 								{/* biome-ignore lint: ignore all rules for this div */}
 								<img
 									alt="eye"
-									style={{
-										position: "absolute",
-										width: "18px",
-										height: "18px",
-										cursor: "pointer",
-										top: "50%",
-										right: "1rem",
-										transform: "translateY(-50%)",
-									}}
+									className="password-visible-toggle"
 									src={
 										isPasswordVisible
 											? "/src/assets/hide_password.png"
@@ -414,39 +467,21 @@ export default function Login() {
 							</div>
 						</div>
 
-						<button
-							type="submit"
-							style={{
-								backgroundColor: "#07a31f",
-								color: "white",
-								borderRadius: "32px",
-								marginTop: "1rem",
-								width: "270px",
-								height: "53px",
-								fontSize: "16px",
-							}}
-						>
+						<button type="submit" className="submit-button">
 							{translations.login.loginText}
 						</button>
 					</form>
 
-					<div
-						style={{
-							marginTop: "1rem",
-							display: "flex",
-							width: "400px",
-							justifyContent: "space-between",
-						}}
-					>
+					<div className="register-container">
 						<a
 							href="https://online.123fakturera.se/register/"
-							style={{ color: "black" }}
+							className="register-link"
 						>
 							{translations.login.registerText}
 						</a>
 						<a
 							href="https://online.123fakturera.se/forgot-password/?email="
-							style={{ color: "black" }}
+							className="forgot-password-link"
 						>
 							{translations.login.forgotPasswordText}
 						</a>
@@ -455,66 +490,29 @@ export default function Login() {
 			</div>
 
 			{/* footer */}
-			<footer
-				style={{
-					paddingTop: "6rem",
-					display: "flex",
-					flexDirection: "column",
-					alignItems: "center",
-					justifyContent: "center",
-				}}
-			>
-				<div
-					style={{
-						display: "flex",
-						alignItems: "center",
-						justifyContent: "space-between",
-						width: "85%",
-						padding: "0px 16px 10px 16px",
-						margin: "auto",
-						borderBottom: "1px solid white",
-					}}
-				>
-					<p style={{ fontSize: "32px", margin: "0" }}>123 Fakturera</p>
-					<ul
-						style={{
-							display: "flex",
-							alignItems: "center",
-							gap: "60px",
-							margin: "0",
-						}}
-					>
-						<li
-							style={{
-								fontWeight: 500,
-								cursor: "pointer",
-								listStyle: "none",
-							}}
-						>
-							{translations.navBar.home}
+			<div className="footer-wrapper">
+				<footer className="footer">
+					<p className="footer-text">123 Fakturera</p>
+					<ul className="footer-list">
+						<li className="footer-list-item">
+							<a href="https://www.123fakturera.se/index.html">
+								{translations.navBar.home}
+							</a>
 						</li>
-						<li
-							style={{
-								cursor: "pointer",
-								fontWeight: 500,
-								listStyle: "none",
-							}}
-						>
-							{translations.navBar.order}
+						<li className="footer-list-item">
+							<a href="https://www.123fakturera.se/bestall.html">
+								{translations.navBar.order}
+							</a>
 						</li>
-						<li
-							style={{
-								cursor: "pointer",
-								fontWeight: 500,
-								listStyle: "none",
-							}}
-						>
-							{translations.navBar.contactUs}
+						<li className="footer-list-item">
+							<a href="https://www.123fakturera.se/kontaktaoss.html">
+								{translations.navBar.contactUs}
+							</a>
 						</li>
 					</ul>
-				</div>
+				</footer>
 				<p>© Lättfaktura, CRO no. 638537, 2025. All rights reserved.</p>
-			</footer>
+			</div>
 		</div>
 	);
 }
